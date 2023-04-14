5cb210c9b0eda5de1ee20fcc2f41eb4f6d9b8e49
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-14 14:00:45 +0200

    Complete navbar for all devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { images, styles, data } from '../constants'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  return (
+    <>
+      <nav className={`${styles.padding} flex justify-between items-center`}>
+        <Link to="/">
+          <img src={images.logoDark} alt="logo" className="w-[200px] h-[25px]" />
+        </Link>
+        <ul className="sm:flex hidden gap-8">
+          {data.navLinks.map((link) => (
+            <Link to={link.path} key={link.id}>
+              <li className="text-[14px] hover:text-peach" onClick={() => setToggleMenu(false)}>
+                {link.link}
+              </li>
+            </Link>
+          ))}
+        </ul>
+        <img src={toggleMenu ? images.iconClose : images.iconHamburger} alt="menu" className="sm:hidden flex gap-8" onClick={() => setToggleMenu(!toggleMenu)} />
+      </nav>
+      {toggleMenu && (
+        <div className="bg-black w-full">
+          <ul className="flex flex-col gap-8 px-4 py-8">
+            {data.navLinks.map((link) => (
+              <Link to={link.path} key={link.id}>
+                <li className="text-[20px] text-white" onClick={() => setToggleMenu(false)}>
+                  {link.link}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Navbar
