75a9d4fde31d84859a7ef4dd307a7fe4dcf9a78a
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-15 17:26:34 +0200

    Complete Cards section

--- src/components/Designs/Cards.jsx
+++ src/components/Designs/Cards.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { styles, data } from '../../constants'
 
-const Cards = () => {
-  return <div>Cards</div>
+const Card = ({ image, title, text }) => (
+  <div className="flex xl:flex-col md:flex-row flex-col xl:w-[350px] xl:h-[478px] md:w-[689px] md:h-[310px] w-[327px] h-[478px]">
+    <img
+      src={image}
+      alt="img"
+      className="xl:w-[350px] xl:h-[320px] md:w-[339px] md:h-[310px] w-[327px] h-[320px]
+    xl:rounded-ss-lg xl:rounded-se-lg xl:rounded-es-none md:rounded-es-xl md:rounded-se-none rounded-ss-lg rounded-se-lg"
+    />
+    <div className="bg-veryLightPeach hover:bg-peach text-peach hover:text-white hover:cursor-pointer flex flex-col items-center justify-center gap-4 xl:rounded-es-lg xl:rounded-ee-lg xl:rounded-se-none md:rounded-se-lg md:rounded-es-none rounded-es-lg rounded-ee-lg p-6" id="hover">
+      <h3 className="tracking-[5px] text-[20px]">{title}</h3>
+      <p className="text-black text-center" id="child">
+        {text}
+      </p>
+    </div>
+  </div>
+)
+
+const Cards = ({ designs = data.webDesigns }) => {
+  return (
+    <section className={`${styles.padding} xl:grid xl:grid-cols-3 flex flex-col items-center md:gap-8 gap-4 mt-20`}>
+      {designs.map((design) => (
+        <Card key={design.title} {...design} />
+      ))}
+    </section>
+  )
 }
 
 export default Cards
