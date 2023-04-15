1e11a69c42653a4091d832256043c7f8257b7c4c
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-15 18:12:19 +0200

    Add props to components

--- src/components/WebWrapper.jsx
+++ src/components/WebWrapper.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Header, Cards, OtherDesigns } from './index'
+import { data } from '../constants'
 
 const WebWrapper = () => {
   return (
     <div>
-      <Header />
-      <Cards />
-      <OtherDesigns />
+      <Header
+        title="Web Design"
+        description="We build websites that serve as powerful marketing tools 
+        and bring memorable brand experiences."
+      />
+      <Cards designs={data.webDesigns} />
+      <OtherDesigns design1="App" design2="Graphic" />
     </div>
   )
 }
