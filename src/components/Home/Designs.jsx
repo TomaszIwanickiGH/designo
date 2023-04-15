cda9ae4be9afa90326831913d74c3eec78289da7
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-15 10:00:26 +0200

    Make little changes to center text on images perfectly

--- src/components/Home/Designs.jsx
+++ src/components/Home/Designs.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 const Designs = () => {
   return (
     <section className={`${styles.padding} flex lg:flex-row flex-col gap-6 mt-12 justify-center`}>
-      <div className="bg-lightPeach rounded-lg">
+      <div className="bg-lightPeach rounded-lg h-fit">
         <div className="relative hover:cursor-pointer hover:opacity-40">
-          <img src={images.imageWebDesignLargeDesktop} alt="web" className="rounded-lg brightness-50 lg:w-fit w-full lg:flex hidden" />
+          <img src={images.imageWebDesignLargeDesktop} alt="web" className="rounded-lg brightness-50 object-cover w-full lg:flex hidden" />
           <img src={images.imageWebDesignTablet} alt="web" className="lg:hidden md:flex hidden w-full rounded-lg brightness-50" />
           <img src={images.imageWebDesignMobile} alt="web" className="md:hidden flex w-full rounded-lg brightness-50" />
-          <div className="absolute lg:top-[42%] lg:left-[27%] md:top-[25%] md:left-[35%] top-[32%] left-[20%]">
-            <h2 className="text-white lg:text-[40px] text-[28px] mb-4">WEB DESIGN</h2>
-            <p className="text-white flex text-[15px] justify-center items-center gap-4">
+          <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h2 className="text-white xl:text-[40px] lg:text-[30px] text-[24px] mb-4">WEB DESIGN</h2>
+            <p className="text-white flex lg:text-[15px] text-[12px] justify-center items-center gap-4">
               VIEW PROJECTS <img src={images.iconRightArrow} className="w-[10px] h-[10px]" />
             </p>
           </div>
@@ -24,9 +24,9 @@ const Designs = () => {
             <img src={images.imageAppDesignDesktop} alt="app" className="rounded-lg brightness-50 lg:w-fit w-full lg:flex hidden" />
             <img src={images.imageAppDesignTablet} alt="app" className="lg:hidden md:flex hidden w-full rounded-lg brightness-50" />
             <img src={images.imageAppDesignMobile} alt="app" className="md:hidden flex w-full rounded-lg brightness-50" />
-            <div className="absolute lg:top-[42%] lg:left-[27%] md:top-[25%] md:left-[35%] top-[32%] left-[20%]">
-              <h2 className="text-white lg:text-[40px] text-[28px] mb-4">APP DESIGN</h2>
-              <p className="text-white flex text-[15px] justify-center items-center gap-4">
+            <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+              <h2 className="text-white xl:text-[40px] lg:text-[30px] text-[24px] mb-4">APP DESIGN</h2>
+              <p className="text-white flex lg:text-[15px] text-[12px] justify-center items-center gap-4">
                 VIEW PROJECTS <img src={images.iconRightArrow} className="w-[10px] h-[10px]" />
               </p>
             </div>
@@ -37,9 +37,9 @@ const Designs = () => {
             <img src={images.imageGraphicDesignDesktop} alt="graphic" className="rounded-lg brightness-50 lg:w-fit w-full lg:flex hidden" />
             <img src={images.imageGraphicDesignTablet} alt="graphic" className="lg:hidden md:flex hidden w-full rounded-lg brightness-50" />
             <img src={images.imageGraphicDesignMobile} alt="graphic" className="md:hidden flex w-full rounded-lg brightness-50" />
-            <div className="absolute lg:top-[35%] lg:left-[20%] md:top-[25%] md:left-[30%] top-[32%] left-[10%]">
-              <h2 className="text-white lg:text-[40px] text-[28px] mb-4">GRAPHIC DESIGN</h2>
-              <p className="text-white flex text-[15px] justify-center items-center gap-4">
+            <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+              <h2 className="text-white xl:text-[40px] lg:text-[30px] text-[24px] mb-4">GRAPHIC</h2>
+              <p className="text-white flex lg:text-[15px] text-[12px] justify-center items-center gap-4">
                 VIEW PROJECTS <img src={images.iconRightArrow} className="w-[10px] h-[10px]" />
               </p>
             </div>
