fa794eed124ab5220d183eaa40682e5334f583e0
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-15 18:19:15 +0200

    Add props to components

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Header, Cards, OtherDesigns } from './index'
+import { data } from '../constants'
 
 const AppWrapper = () => {
   return (
     <div>
-      <Header />
-      <Cards />
-      <OtherDesigns />
+      <Header
+        title="App Design"
+        description="Our mobile designs bring intuitive digital solutions
+        to your customers right at their fingertips."
+      />
+      <Cards designs={data.appDesigns} />
+      <OtherDesigns design1="Web" design2="Graphic" />
     </div>
   )
 }
--- src/components/GraphicWrapper.jsx
+++ src/components/GraphicWrapper.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Header, Cards, OtherDesigns } from './index'
+import { data } from '../constants'
 
 const GraphicWrapper = () => {
   return (
     <div>
-      <Header />
-      <Cards />
-      <OtherDesigns />
+      <Header
+        title="Graphic Design"
+        description="We deliver eye-catching branding materials that are 
+        tailored to meet your business objectives."
+      />
+      <Cards designs={data.graphicDesigns} />
+      <OtherDesigns design1="Web" design2="App" />
     </div>
   )
 }
