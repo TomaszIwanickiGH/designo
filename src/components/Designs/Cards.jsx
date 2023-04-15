46b3ed6040a83f17aa73ac76003073430c68d653
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-15 18:19:04 +0200

    Small Fixes

--- src/components/Designs/Cards.jsx
+++ src/components/Designs/Cards.jsx
@@ -9,7 +9,7 @@ const Card = ({ image, title, text }) => (
       className="xl:w-[350px] xl:h-[320px] md:w-[339px] md:h-[310px] w-[327px] h-[320px]
     xl:rounded-ss-lg xl:rounded-se-lg xl:rounded-es-none md:rounded-es-xl md:rounded-se-none rounded-ss-lg rounded-se-lg"
     />
-    <div className="bg-veryLightPeach hover:bg-peach text-peach hover:text-white hover:cursor-pointer flex flex-col items-center justify-center gap-4 xl:rounded-es-lg xl:rounded-ee-lg xl:rounded-se-none md:rounded-se-lg md:rounded-es-none rounded-es-lg rounded-ee-lg p-6" id="hover">
+    <div className="bg-veryLightPeach hover:bg-peach text-peach hover:text-white hover:cursor-pointer flex flex-col items-center justify-center gap-4 xl:rounded-es-lg xl:rounded-ee-lg xl:rounded-se-none md:rounded-se-lg md:rounded-es-none rounded-es-lg rounded-ee-lg p-6 xl:min-h-[168px]" id="hover">
       <h3 className="tracking-[5px] text-[20px]">{title}</h3>
       <p className="text-black text-center" id="child">
         {text}
