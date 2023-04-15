31a64be7c56cae4dbdacf6d39882888f8515c33f
Tomasz <tomasz.iwanicki@onet.pl>
2023-04-15 16:08:23 +0200

    Complete Header section

--- src/components/Designs/Header.jsx
+++ src/components/Designs/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { styles, images } from '../../constants'
 
-const Header = () => {
-  return <div>Header</div>
+const Header = ({ title, description }) => {
+  return (
+    <section className={`${styles.paddingSmall}`}>
+      <div className={`${styles.padding} bg-peach sm:rounded-lg flex flex-col items-center justify-center lg:gap-8 gap-6`} style={{ backgroundImage: `url(${images.bgCTA})` }}>
+        <h2 className="text-white lg:text-[48px] text-[32px]">{title}</h2>
+        <p className="text-white xl:w-1/2 text-center">{description}</p>
+      </div>
+    </section>
+  )
 }
 
 export default Header
